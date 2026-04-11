b43368672d4d181bee37b61f0e56075c7c9ed13a
ranzaWARE <tua-email-di-github@esempio.com>
2026-04-11 18:04:15 +0200

    fix nginx

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 set -e
-
-apk add --no-cache openssl
-
 mkdir -p /etc/nginx/certs
-
 if [ ! -f /etc/nginx/certs/quetza.crt ]; then
-  echo "Generating self-signed SSL certificate..."
-  openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
-    -keyout /etc/nginx/certs/quetza.key \
-    -out    /etc/nginx/certs/quetza.crt \
-    -subj   "/CN=quetza/O=Graphimecc/C=IT" \
-    -addext "subjectAltName=IP:${SERVER_IP:-127.0.0.1},DNS:localhost"
-  echo "Certificate generated."
+    echo "Generating SSL certificate for IP: ${SERVER_IP:-127.0.0.1}"
+    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
+        -keyout /etc/nginx/certs/quetza.key \
+        -out    /etc/nginx/certs/quetza.crt \
+        -subj   "/CN=quetza/O=Graphimecc/C=IT" \
+        -addext "subjectAltName=IP:${SERVER_IP:-127.0.0.1},DNS:localhost"
+    echo "Certificate generated."
 fi
-
 exec nginx -g 'daemon off;'
